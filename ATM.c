2ef430fe8cdbcc4a29420fd60d2f953a582f2eff
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-21 15:57:37 +0300

    Remove Windows-specific dependencies and make code cross-platform.

--- ATM.c
+++ ATM.c
@@ -1,12 +1,6 @@
 #include<stdio.h>
-#include<windows.h>
 #include<string.h>
 #include<stdlib.h>
-#define RED 12		// color definitions
-#define GREEN 10
-#define YELLOW 14
-#define WHITE 15
-#define BLUE 9
 
 struct User {
 	char username[20];
@@ -27,9 +21,6 @@ static char accountUsername[20], accountPassword[20];
 static float accountBalance;
 
 int main(void) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
-
 	static char choice;
 	static char answer;
 
@@ -57,16 +48,11 @@ int main(void) {
 }
 
 void Login(int lineCount) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 	static int isLogged = 0;
 	int i, j;
 	struct User user;
-
 	
 	for(i = 0; i < 3; i++) {
-		SetConsoleTextAttribute(hConsole, 9);
-
 		printf("\nUsername: ");
 		scanf("%s", accountUsername);
 		printf("Password: ");
@@ -76,7 +62,6 @@ void Login(int lineCount) {
 			
 			FILE *file = fopen("users.txt", "r");
 			if(file == NULL) {
-				SetConsoleTextAttribute(hConsole, 12);
 				perror("There was an error opening the file.\n");
 				exit(1);
 			}
@@ -90,34 +75,26 @@ void Login(int lineCount) {
 		if(isLogged != 0) {
 			break;
 		}	
-		SetConsoleTextAttribute(hConsole, 14);
 		printf("\n\t\tYou have %d attempts left.\n", (2 - i));
 	}
 	
 	if(isLogged == 0) {
-			SetConsoleTextAttribute(hConsole, 12);
 			printf("\nYou have reached the maximum number of attempts. Exiting the program.\n");
-			SetConsoleTextAttribute(hConsole, 9);
 			system("pause");
 			exit(1);
 		}
 
-	SetConsoleTextAttribute(hConsole, 10);
 	printf("\n\t\tLogin successful.\n\n");
-	SetConsoleTextAttribute(hConsole, 9);
 	system("pause");
 	system("cls");
 }
 
 int LineCount() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 	int lineCount = 0;
 	char ch;
 
 	FILE *file = fopen("users.txt", "r");
 	if(file == NULL) {
-		SetConsoleTextAttribute(hConsole, 12);
 		perror("There was an error opening the file.\n");
 		exit(1);
 	}
@@ -135,8 +112,6 @@ int LineCount() {
 }
 
 void Menu(char* choice) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
 	printf(	"\n1- Check balance\n"
 	        "2- Deposit money\n"
 	        "3- Withdraw money\n"
@@ -146,54 +121,36 @@ void Menu(char* choice) {
 }
 
 void Accept(char *answer) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
 
 	printf("\nDo you want to continue with the operation?\n");
-	SetConsoleTextAttribute(hConsole, 10);
 	printf("Continue = 1 ");
-	SetConsoleTextAttribute(hConsole, 12);
 	printf("Cancel = 0\n");
-	SetConsoleTextAttribute(hConsole, 9);
 	printf("Answer: ");
 	scanf(" %s", answer);
 }
 
 void CheckBalance() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
 	printf("\n\n-----------------------------------------------------\n");
-	SetConsoleTextAttribute(hConsole, 15);
 	printf("\nBalance: ");
-	SetConsoleTextAttribute(hConsole, 10);
 	printf("%.2f", accountBalance);
-	SetConsoleTextAttribute(hConsole, 9);
 	printf("\n\n-----------------------------------------------------\n\n");
 	system("pause");
 	system("cls");
 }
 
 void Deposit() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
-
 	float deposited;
 
 	printf("\nEnter the amount of money you want to deposit: ");
 	scanf("%f", &deposited);
 
 	accountBalance += deposited;
-
-	SetConsoleTextAttribute(hConsole, 10);
 	printf("\n\n\t\tTransaction successful.");
 
 	CheckBalance();
 }
 
 void Withdraw() {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 9);
-
 	float withdrawn, temp;
 
 	printf("\nEnter the amount of money you want to withdraw: ");
@@ -203,10 +160,8 @@ void Withdraw() {
 
 	if (temp >= 0) {
 		accountBalance -= withdrawn;
-		SetConsoleTextAttribute(hConsole, 10);
 		printf("\n\n\t\tTransaction successful.");
 	} else {
-		SetConsoleTextAttribute(hConsole, 12);
 		printf("\n\n\t\tYour balance is insufficient.");
 	}
 
@@ -214,15 +169,12 @@ void Withdraw() {
 }
 
 void UpdateFile(int lineCount) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 	int i;
 	struct User account;
 
 	FILE *file1 = fopen("users.txt", "r");
 	FILE *file2 = fopen("newUsers.txt", "w");
 	if(file1 == NULL || file2 == NULL) {
-		SetConsoleTextAttribute(hConsole, 12);
 		perror("There was an error opening the file.\n");
 		exit(1);
 	}
